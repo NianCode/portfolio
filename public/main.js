929ee8197073447118a8b0f920d78563ff4e594d
NianCode <nicolash.contato@gmail.com>
2024-03-15 02:34:01 -0300

    -

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 
 window.onscroll = function () {
     var navbar = document.querySelector('.navbar');
-    if (window.pageYOffset > 160) { /* Defina a quantidade de scroll aqui */
+    if (window.pageYOffset > 10) { /* Defina a quantidade de scroll aqui */
         navbar.classList.add('solid');
     } else {
         navbar.classList.remove('solid');
